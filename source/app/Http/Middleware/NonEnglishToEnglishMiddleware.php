5588beb5237862780edcd6bec516047eb2b3c87d
ali <alibabaei037@gmail.com>
2022-08-07 01:18:59 +0430

    add non english char middleware

---
+++ source/app/Http/Middleware/NonEnglishToEnglishMiddleware.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class NonEnglishToEnglishMiddleware
+{
+
+    /**
+     * @var array
+     */
+    protected array $except = [];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): Response|RedirectResponse
+    {
+        $except = array_merge($this->except, array_slice(func_get_args(), 2));
+        $request->merge($this->process($request->except($except)));
+        return $next($request);
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     */
+    protected function process(array $data): array
+    {
+        array_walk_recursive(
+            $data,
+            function (&$value) {
+                $value = $this->processValue($value);
+            }
+        );
+
+        return $data;
+    }
+
+    /**
+     * @param mixed $value
+     * @return mixed
+     */
+    protected function processValue(string $value): mixed
+    {
+        $arabicDigits = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+        $persianDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+        $englishDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+        $value = str_replace($arabicDigits, $englishDigits, $value);
+        return str_replace($persianDigits, $englishDigits, $value);
+    }
+}
---
+++ source/tests/Unit/Middleware/NonEnglishToEnglishMiddlewareTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit\Middleware;
+
+use App\Http\Middleware\NonEnglishToEnglishMiddleware;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use PHPUnit\Framework\TestCase;
+
+class NonEnglishToEnglishMiddlewareTest extends TestCase
+{
+    public function test_persian_numbers_to_english_numbers()
+    {
+        $request = new Request;
+
+        $request->merge([
+            'attr' => '۱۲۳۴'
+        ]);
+
+        $middleware = new NonEnglishToEnglishMiddleware();
+
+        $middleware->handle($request, function ($req) {
+            $this->assertEquals('1234', $req->attr);
+            return new Response();
+        });
+    }
+
+    public function test_arabic_numbers_to_english_numbers()
+    {
+        $request = new Request;
+
+        $request->merge([
+            'attr' => '٠١٢٣٤٥'
+        ]);
+
+        $middleware = new NonEnglishToEnglishMiddleware();
+
+        $middleware->handle($request, function ($req) {
+            $this->assertEquals('012345', $req->attr);
+            return new Response();
+        });
+    }
+
+}
